1a66a4ac915f1d9c39224d33ba16f5cdb13a0e12
Paula Iacoban <i.p.iacoban@gmail.com>
2022-04-30 15:55:31 +0200

    Fix requirements

--- src/main.py
+++ src/main.py
@@ -2,15 +2,13 @@ import pandas as pd
 import numpy as np
 import os
 
-# pd.set_option('display.float_format',  '{:,.5f}'.format)
-
 frame = []
-data_folder = os.getcwd() + '/CGI-alles/Data/PMP Data/'
+data_folder = 'CGI-alles/Data/PMP Data/'
 
 df = pd.DataFrame(frame)
 
 for datafile in os.listdir(data_folder):
-    if os.path.isfile(datafile):
-        df = pd.read_csv(datafile)
-        index = df.index
-        number_of_rows = len(index)
+    print(datafile)
+
+    df = pd.read_csv(data_folder + datafile)
+    print(df)
