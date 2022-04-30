0cf6bd3c0266d73c63564cebb46cd2900c2aabd4
Paula Iacoban <i.p.iacoban@gmail.com>
2022-04-30 16:44:21 +0200

    Get unique values in a col

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import numpy as np
 import os
+# import matplotlib.pyplot as plt
+
+
+# def get_boxplot(figname, data, labels):
+
+#     fig = plt.figure(figsize =(10, 7))
+#     # Creating axes instance
+
+#     # Creating plot
+#     bp = plt.boxplot(data)
+
+#     # show plot
+#     plt.savefig(figname+".jpeg")
+#     plt.show()
+
+
+
+# pd.set_option('display.float_format',  '{:,.5f}'.format)
 
 frame = []
 data_folder = 'CGI-alles/Data/PMP Data/'
@@ -9,6 +27,14 @@ df = pd.DataFrame(frame)
 
 for datafile in os.listdir(data_folder):
     print(datafile)
+    df = pd.read_csv(data_folder + datafile, sep=";")
+    attr = df["Attribuut"].unique()
+    asset = df["Assetnaam"].unique()
 
-    df = pd.read_csv(data_folder + datafile)
-    print(df)
+    print(attr)
+    print(asset)
+
+
+
+    break
+
