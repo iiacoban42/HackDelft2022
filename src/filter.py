cf9c7d2be41a4ede296daba8b7b788aec2def5d5
AndrewStefan <andrei.al.stefan@gmail.com>
2022-05-01 10:38:04 +0200

    fix filter

--- src/filter.py
+++ src/filter.py
@@ -8,21 +8,30 @@ data_folder = 'CGI-alles/Data/PMP Data/'
 
 data = os.listdir(data_folder)
 
-it = iter(data)
-for datafile in it:
-    next_dafatile = next(it)
-    print (datafile, next_dafatile)
+switch_names = []
 
-    name = datafile.split("_Motorstroomdata")[0]
+for filename in data:
+    if filename.__contains__("_Motorstroomdata"):
+        name = filename.split("_Motorstroomdata")[0]
+        switch_names.append(name)
 
-    print(name)
+# print(switch_names)
+# print(len(switch_names))
 
-    df_motor = pd.read_csv(data_folder + datafile, sep=";")
+# it = iter(data)
+# for datafile in it:
+for name in switch_names:
+    motor_file = f'{name}_Motorstroomdata.csv'
+    other_file = f'{name}_otherdata.csv'
+    print(motor_file)
+    print(other_file)
+
+    df_motor = pd.read_csv(data_folder + motor_file, sep=";")
 
     df_motor = df_motor.drop("Assetnaam", 1)
 
     # print(df_motor)
-    df_other = pd.read_csv(data_folder + next_dafatile, sep=";")
+    df_other = pd.read_csv(data_folder + other_file, sep=";")
 
     df_other = df_other.drop("Assetnaam", 1)
 
