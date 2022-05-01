e2f0ff04926e0628b18df03e2636628c0e57e537
AndrewStefan <andrei.al.stefan@gmail.com>
2022-05-01 11:29:39 +0200

    fix plots

--- src/main.py
+++ src/main.py
@@ -29,48 +29,62 @@ def get_plot_data(plot_name, attribute, df):
     data = []
     boxplot_df = pd.DataFrame([])
 
+    # print(attr)
+
     for att in attr:
         # filter rows by attribute
+        # print(att)
+
         new_df = df[df[attribute]==att]
         # get values
         new_data = new_df["Value"]
         data.append(new_data)
         boxplot_df[att] = new_data
 
-    daaata = {
-        attr[0]: data[0],
-        attr[1]: data[1]
+        daaata = {
+            attr[0]: data[0]
+        }
+        # boxplot_df[factor_config[0]] =
+        # boxplot_df[factor_config[1]] = data[1]
 
-    }
-    # boxplot_df[factor_config[0]] =
-    # boxplot_df[factor_config[1]] = data[1]
+        boxplot_df = pd.DataFrame(new_df["Value"])
+        # print(data)
+        print(boxplot_df)
 
-    boxplot_df = pd.DataFrame(daaata)
-    # print(data)
-    print(boxplot_df)
-
-    myFig = plt.figure()
-    bp = boxplot_df.boxplot()
-    myFig.savefig(plot_name+".jpg", format="jpg")
+        myFig = plt.figure()
+        bp = boxplot_df.boxplot()
+        att = att.replace("/", "")
+        print(f"{plot_name}_{att}.jpg")
+        myFig.savefig(f"{plot_name}_{att}.jpg", format="jpg")
 
 def check_equal_dates(date_1: DateTime, date_2: DateTime):
     return date_1.date() == date_2.date()
 
+if __name__ == '__main__':
 
-frame = []
-# os.chdir("../")
-data_folder = 'src/combined_data/'
+    frame = []
+    # os.chdir("../")
+    data_folder = 'src/combined_data/'
 
-df = pd.DataFrame(frame)
+    df = pd.DataFrame(frame)
 
-files = os.listdir(data_folder)
+    files = os.listdir(data_folder)
 
-regex = ".*[a-zA-Z].*"
+    regex = ".*[a-zA-Z].*"
 
+    for  datafile in files[0:1]:
 
-df = pd.read_csv("src/combined_data/AH$2113_data.csv")
+        df = pd.read_csv(data_folder + datafile)
 
-df['Value'] = df['Value'].apply(lambda x: not re.match(regex, str(x))).index
-# print(df["Attribuut"].unique())
-get_plot_data("plt1", "Attribuut", df)
-# get_plot_data("plt2", "Assetnaam", df)
+        df['Value'] = df['Value'].apply(lambda x: x if not re.match(regex, str(x)) else "DELETE")
+
+        indexNames = df[df['Value'] == 'DELETE'].index
+
+        df.drop(indexNames, inplace=True)
+
+        df = df.astype({'Value': 'float'})
+
+        print(df)
+        # print(df["Attribuut"].unique())
+        get_plot_data(f"{datafile[:-4]}", "Attribuut", df)
+        # get_plot_data("plt2", "Assetnaam", df)
